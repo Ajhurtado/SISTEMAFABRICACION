996dd91cfeacbad52f9f4e4059ea33acb2134d9b
Ajhurtado <ajhurtadop@uce.edu.ec>
2024-07-29 04:15:25 -0500

    ProyectoFinalParte1

---
+++ src/main/java/ec/edu/uce/demo/ProyectoNuevo1Application.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoNuevo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoNuevo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/uce/demo/ProyectoNuevo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoNuevo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
